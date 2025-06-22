b28fae02b8e3746b711059d2fee17ef40879143b
Haowei Guo <Guo-Haowei@users.noreply.github.com>
2025-06-23 08:57:29 +0900

    more

--- apps/match-ui/vite.config.js
+++ apps/match-ui/vite.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite';
 import path from 'path';
 
@@ -5,9 +6,7 @@ export default defineConfig({
   server: {
     fs: {
       allow: [
-        // eslint-disable-next-line no-undef
         path.resolve(__dirname, '../../pkg'),
-        // eslint-disable-next-line no-undef
         path.resolve(__dirname, 'src'),
       ]
     }
