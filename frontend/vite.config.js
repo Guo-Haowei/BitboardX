898f532d1a61163d652ac316874da6f389a64a8c
Haowei Guo <Guo-Haowei@users.noreply.github.com>
2026-09-04 22:17:32 +0900

    update vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,14 @@
 /* eslint-disable no-undef */
-import { defineConfig } from 'vite';
-import path from 'path';
+import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
-  base: '/pages/chess/',
+  base: "/pages/chess/",
   server: {
     host: true,
     port: 8000,
     fs: {
-      allow: [
-        path.resolve(__dirname, '../pkg'),
-        path.resolve(__dirname, 'src'),
-      ]
-    }
-  }
+      allow: [path.resolve(__dirname, "../pkg"), path.resolve(__dirname, ".")],
+    },
+  },
 });
