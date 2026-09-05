0c457b1aecd6b89dde071079ed1b44b1eef87d75
Haowei Guo <Guo-Haowei@users.noreply.github.com>
2026-09-05 21:31:12 +0900

    Introduce React (#43)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,19 @@
 /* eslint-disable no-undef */
-import { defineConfig } from 'vite';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
-  base: '/pages/chess/',
+  base: "/pages/chess/",
+
+  plugins: [react()],
+
   server: {
     host: true,
     port: 8000,
+
     fs: {
-      allow: [
-        path.resolve(__dirname, '../pkg'),
-        path.resolve(__dirname, 'src'),
-      ]
-    }
-  }
+      allow: [path.resolve(__dirname), path.resolve(__dirname, "../pkg")],
+    },
+  },
 });
