c7db46d48cdc3e1bbaad90e38de3e68b628103c1
Haowei Guo <Guo-Haowei@users.noreply.github.com>
2026-09-15 21:47:20 +0900

    Sidebar (#46)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
 import { useEffect, useRef, useState } from "react";
 import * as Chess from "./chess";
+
 import ChessBoard from "./components/ChessBoard";
-import PlayerSelector, { PlayerType } from "./components/PlayerSelector";
+import PlayerSelector, {
+  type PlayerType,
+} from "./components/PlayerSelector";
+import Sidebar from "./components/Sidebar";
 import TopBar from "./components/TopBar";
 
 type ChessController = Awaited<
@@ -21,6 +25,8 @@ export default function App() {
     useState<PlayerType>("bot");
 
   const [fen, setFen] = useState("");
+
+  const [newGameOpen, setNewGameOpen] = useState(false);
 
   function createPlayer(type: PlayerType): Chess.Player {
     if (type === "bot") {
@@ -42,14 +48,15 @@ export default function App() {
   }
 
   useEffect(() => {
-    if (!canvasRef.current) {
+    const canvas = canvasRef.current;
+
+    if (!canvas) {
       return;
     }
 
-    async function initialize() {
+    async function initialize(canvas: HTMLCanvasElement) {
       const chessController = await Chess.initialize({
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        canvas: canvasRef.current!,
+        canvas,
       });
 
       setController(chessController);
@@ -61,90 +68,105 @@ export default function App() {
       );
     }
 
-    initialize();
+    initialize(canvas);
   }, []);
 
   return (
     <>
       <TopBar />
-      <div className="chess-app">
-        {/* Header */}
-        <div className="header">
-          <div className="player you">
-            <img
-              className="avatar"
-              src="https://lichess1.org/assets/piece/cburnett/wK.svg"
-              alt="White"
-            />
 
-            <div className="info">
-              <div>
-                {whitePlayer === "bot" ? "Bot" : "Human"}
+      <main className="game-layout">
+        <section className="game-column">
+          <div className="chess-app">
+            {/* Header */}
+            <div className="header">
+              <div className="player you">
+                <img
+                  className="avatar"
+                  src="https://lichess1.org/assets/piece/cburnett/wK.svg"
+                  alt="White"
+                />
+
+                <div className="info">
+                  <div>
+                    {whitePlayer === "bot"
+                      ? "Bot"
+                      : "Human"}
+                  </div>
+                </div>
+              </div>
+
+              <div className="player opp">
+                <img
+                  className="avatar"
+                  src="https://lichess1.org/assets/piece/cburnett/bK.svg"
+                  alt="Black"
+                />
+
+                <div className="info">
+                  <div>
+                    {blackPlayer === "bot"
+                      ? "Bot"
+                      : "Human"}
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="player opp">
-            <img
-              className="avatar"
-              src="https://lichess1.org/assets/piece/cburnett/bK.svg"
-              alt="Black"
+            {/* Chess board */}
+            <ChessBoard canvasRef={canvasRef} />
+
+            {/* Temporary old new-game controls */}
+            <PlayerSelector
+              color="White"
+              value={whitePlayer}
+              onChange={setWhitePlayer}
             />
 
-            <div className="info">
-              <div>
-                {blackPlayer === "bot" ? "Bot" : "Human"}
-              </div>
-            </div>
+            <p />
+
+            <PlayerSelector
+              color="Black"
+              value={blackPlayer}
+              onChange={setBlackPlayer}
+            />
+
+            <p />
+
+            <input
+              type="text"
+              id="fen-input"
+              value={fen}
+              onChange={(event) =>
+                setFen(event.target.value)
+              }
+              style={{ width: 300 }}
+              placeholder="please enter FEN here"
+            />
+
+            <p />
+
+            <button
+              id="start-button"
+              onClick={startGame}
+              disabled={!controller}
+            >
+              Go!
+            </button>
+
+            {newGameOpen && (
+              <p>
+                New Game modal will go here next.
+              </p>
+            )}
           </div>
-        </div>
+        </section>
 
-        {/* Chess board */}
-        <ChessBoard canvasRef={canvasRef} />
-
-        {/* Controls */}
-        <div className="controls">
-          <button
-            id="resume-button"
-            onClick={() => controller?.resume()}
-          >
-            Resume
-          </button>
-
-          <button
-            id="undo-button"
-            onClick={() => controller?.undo()}
-          >
-            Undo
-          </button>
-        </div>
-
-        <PlayerSelector color="White" value={whitePlayer} onChange={setWhitePlayer} />
-        <p />
-        <PlayerSelector color="Black" value={blackPlayer} onChange={setBlackPlayer} />
-
-        <p />
-
-        {/* FEN */}
-        <input
-          type="text"
-          id="fen-input"
-          value={fen}
-          onChange={(event) => setFen(event.target.value)}
-          style={{ width: 300 }}
-          placeholder="please enter FEN here"
+        <Sidebar
+          onNewGame={() => setNewGameOpen(true)}
+          onUndo={() => controller?.undo()}
         />
-
-        <p />
-
-        <button
-          id="start-button"
-          onClick={startGame}
-          disabled={!controller}
-        >
-          Go!
-        </button>
-      </div>
+      </main>
     </>
   );
 }
